hashicorp/boundary

feat(schema): Add support for migration hooks
This adds the ability to define hooks that will run prior to a schema
migration. The hooks should only be used when a migration would fail due
to data being in an invalid state, likely due to a bug in the previous
schema definition. For example if a column was missing a constraint, and
a new migration is attempting to add the constraint, there might already
be values that violate the new constraint. In such cases, we would want
the user to make a decision to address the data, and need to provide
them with details about what data is problematic in case they want to
manually address the problem. To help the user, if there is a sane way
to address the data, we should provide an option to automatically
"repair" the data, allowing the migration to continue. However, this
still should be an explicit opt-in from the user, and we need to clearly
describe what the repair option would do to the user so they understand
the consequences of opting in.

To accomplish this each migration can be provided a hook that contains a
CheckFunc, RepairFunc, and RepairDescription. These functions must
operate at the point-in-time of the migration immediately preceding the
current migration, as as such they should not use and domain packages or
repositories, instead they should use direct sql with the sql library.
The description is used to explain what the RepairFunc will do.

Hooks should be defined in `internal/db/schema/migrations/oss/oss.go`
and registered with the schema edition.

See the example in `internal/db/schema/manager_example_test.go` for more
details.

internal/db/schema/internal/migration/migration.go
@@ -1,0 +1,38 @@
+package migration
+
+import (
+	"context"
+	"database/sql"
+)
+
+// CheckFunc is a function that checks the state of data in the database to
+// determine if a migration will fail, and if so to report the data that is
+// problematic so it can be fixed.
+type CheckFunc func(context.Context, *sql.Tx) error
+
+// RepairFunc is a function that alters data in the database to resolve issues
+// that would prevent a migration from successfully running.
+type RepairFunc func(context.Context, *sql.Tx) (string, error)
+
+// Hook provides a set of functions that allow for executing checks prior to
+// executing migration statements.
+type Hook struct {
+	CheckFunc  CheckFunc
+	RepairFunc RepairFunc
+
+	// RepairDescription will describe what change running the repair function
+	// would perform.
+	RepairDescription string
+}
+
+// Migration is a set of statements that will alter the database structure or
+// or data.
+type Migration struct {
+	Statements []byte
+	Edition    string
+	Version    int
+	PreHook    *Hook
+}
+
+// Migrations are a set of migrations by version.
+type Migrations map[int]Migration

zocog/hcp-boundary

internal/db/schema/migration/migration.go
@@ -1,0 +1,44 @@
+package migration
+
+import (
+	"context"
+	"database/sql"
+)
+
+// Problems are reports of data issues that were identified by a CheckFunc.
+type Problems []string
+
+// Repairs are reports of changes made to data by a RepairFunc.
+type Repairs []string
+
+// CheckFunc is a function that checks the state of data in the database to
+// determine if a migration will fail, and if so to report the data that is
+// problematic so it can be fixed.
+type CheckFunc func(context.Context, *sql.Tx) (Problems, error)
+
+// RepairFunc is a function that alters data in the database to resolve issues
+// that would prevent a migration from successfully running.
+type RepairFunc func(context.Context, *sql.Tx) (Repairs, error)
+
+// Hook provides a set of functions that allow for executing checks prior to
+// executing migration statements.
+type Hook struct {
+	CheckFunc  CheckFunc
+	RepairFunc RepairFunc
+
+	// RepairDescription will describe what change running the repair function
+	// would perform.
+	RepairDescription string
+}
+
+// Migration is a set of statements that will alter the database structure or
+// or data.
+type Migration struct {
+	Statements []byte
+	Edition    string
+	Version    int
+	PreHook    *Hook
+}
+
+// Migrations are a set of migrations by version.
+type Migrations map[int]Migration